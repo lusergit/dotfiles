90e3e17f5126171a13760993da3f2482619c799c
Luca Zaninotto <lucazanny@gmail.com>
2025-01-20 09:40:38 +0100

    chore(fish): new aliases and asdf
    - incorporated `asdf` for fedora systems
    - `et` and `ec` aliases for terminal and graphical emacs clients
    
    Signed-off-by: Luca Zaninotto <lucazanny@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,11 +7,17 @@ starship init fish | source
 source $HOME/.atuin/bin/env.fish
 atuin init fish | source
 
-# Aliases
-alias ls="ls -gh --color=auto"
-alias la="ls -gha --color=auto"
+# asdf
+source ~/.asdf/asdf.fish
 
+# Aliases
 alias estatus="systemctl --user status emacs"
 alias estart="systemctl --user start emacs"
 alias estop="systemctl --user stop emacs"
 alias erestart="systemctl --user restart emacs"
+
+alias et="emacsclient -t"
+alias ec="emacsclient -c"
+
+alias ls="exa -1lxXh --smart-group --git"
+alias la="exa -1lxXha --smart-group --git"
