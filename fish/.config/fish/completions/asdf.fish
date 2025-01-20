529ffc78d4245094f0526500c0f4690bb5ce5426
Luca Zaninotto <lucazanny@gmail.com>
2025-01-20 11:49:20 +0100

    chore(fish): asdf completion and fish_path
    Signed-off-by: Luca Zaninotto <lucazanny@gmail.com>

---
+++ fish/.config/fish/completions/asdf.fish
@@ -0,0 +1,1 @@
+/home/luser/.asdf/completions/asdf.fish
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,15 @@ starship init fish | source
 source $HOME/.atuin/bin/env.fish
 atuin init fish | source
 
+# path
+fish_add_path $HOME/.local/bin
+
+# asdf
+source $HOME/.asdf/asdf.fish
+
+# GPG
+set -gx GPG_TTY (tty)
+
 # Aliases
 alias ls="ls -gh --color=auto"
 alias la="ls -gha --color=auto"
@@ -15,3 +24,6 @@ alias estatus="systemctl --user status emacs"
 alias estart="systemctl --user start emacs"
 alias estop="systemctl --user stop emacs"
 alias erestart="systemctl --user restart emacs"
+
+alias ec="emacsclient -c"
+alias et="emacsclient -t"
